b4a1895e0a83415b63e3fa6f10edee911e3affcb
Jordan Petersen <crab8011@gmail.com>
2019-08-22 18:25:30 -0600

    Added in a basic Command class for more easily handled and dynamic communication between server and client.

---
+++ CardGameSerfver/src/space/crab8012/cardgameplayer/gameobjects/ServerCommand.java
@@ -0,0 +1,33 @@
+package space.crab8012.cardgameplayer.gameobjects;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/*
+ *  ServerCommand
+ *
+ *  This will eventually be moved into a separate library for making easier Java servers.
+ *  This class should also remain relatively unchanged, due to it's simple nature.
+ *  It is a class intended to transport large amounts of data that can easily be interpreted
+ *  by both the client and the server.
+ *
+ * Both the client and the server should have a shared list of commands, as well as how to deal with them.
+ */
+
+public class ServerCommand implements Serializable {
+    private String command;
+    private ArrayList<Object> payload;
+
+    public ServerCommand(String command, ArrayList<Object> payload){
+        this.command = command;
+        this.payload = payload;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public ArrayList<Object> getPayload() {
+        return payload;
+    }
+}
